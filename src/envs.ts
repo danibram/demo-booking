82531bb0eb5ae0acc37870a4657ae84747196252
dbr <info@dbr.io>
2020-03-15 16:01:32 +0100

    added endpoint for access control

--- src/envs.ts
+++ src/envs.ts
@@ -1,3 +1,4 @@
 export const PORT = process.env.PORT || '3001'
-
 export const IS_TESTING_ENV = process.env.NODE_ENV === 'test'
+export const AC_SERVICE_WWW =
+    process.env.AC_SERVICE_WWW || 'access-control:3001'
